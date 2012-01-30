10799f051dfebccb54fe11985a0f9b1a7913876c
Alex Muravya <asm at sfw dot su>
2012-01-30 14:32:35 +0400

    Small Commit for git.php: added line separator
    Signed-off-by: Alex Muravya <<asm at sfw dot su>>

--- git.php
+++ git.php
@@ -18,7 +18,7 @@ and open the template in the editor.
         GitHub repo: <a href="https://github.com/sfwweb/asm.sfw.su">https://github.com/sfwweb/asm.sfw.su</a><br>
         GitHub Repo is master.
         Actual site <a href="http://asm.sfw.su">http://asm.sfw.su</a> clones master just after my commits review.<br>
-        It is highly recommended to do your branch. Thank you!
+        It is highly recommended to do your branch. Thank you!<br>
         You can contact me by email: webteam@sfw.su
     </body>
 </html>
