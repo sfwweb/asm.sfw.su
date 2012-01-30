3b3550552aae510b664e3aa323a08152a70c3677
Alex Muravya <asm at sfw dot su>
2012-01-30 14:00:32 +0400

    Added git.php.
    Signed-off-by: Alex Muravya <<asm at sfw dot su>>

---
+++ git.php
@@ -0,0 +1,24 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>About Git</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <h1>About My Git</h1>
+        </br>
+        Since now we use Github.<br>
+        GitHub repo: <a href="https://github.com/sfwweb/asm.sfw.su">https://github.com/sfwweb/asm.sfw.su</a><br>
+        GitHub Repo is master.
+        Actual site <a href="http://asm.sfw.su">http://asm.sfw.su</a> clones master just after my commits review.<br>
+        It is highly recommended to do your branch. Thank you!
+        You can contact me by email: webteam@sfw.su
+    </body>
+</html>
