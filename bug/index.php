6866eca62a799736ceec286596b6d40a6e1d4537
Alex Muravya <asm at sfw dot su>
2012-01-31 22:08:15 +0400

    Restored some files from backup
    Signed-off-by: Alex Muravya <<asm at sfw dot su>>

---
+++ bug/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "Ready yet...sorry";
+echo "<br>";
+echo "Will be here very soon...";
+?>
--- index.php
+++ index.php
@@ -4,6 +4,5 @@
 <h1>Test Server works!</h1>
 <p>This is the default web page for test web server.</p>
 <p>The web server software is running but no content has been added, yet.</p>
-<p>We use Git!</p> 
 </body>
 </html>
